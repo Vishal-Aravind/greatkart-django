0c3e3691c6aee08c83aebb0b513b3e9603fb6762
Vishal <vishalaravind03@gmail.com>
2024-07-08 09:21:05 +0530

    variation adding to cart

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',          # we are using context_processors here because you can call menu_links in any template
+                'carts.context_processors.counter',
             ],
         },
     },
